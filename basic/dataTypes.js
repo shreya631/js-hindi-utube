5f78f690b3f0e8e58a926c0ddbf6b05aee1f1f9b
shreya631 <74915529+shreya631@users.noreply.github.com>
2024-04-30 00:20:52 +0000

    datatypes

---
+++ basic/dataTypes.js
@@ -0,0 +1,29 @@
+"use strict";  // treat all js code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+let id = 144553
+let accountEmail = "hitesh@google.com"
+
+console.log(3
+     + 3)  // code readability should be high
+
+// console.log("Ram ji")
+
+//------ Non premitive data type
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefine =>
+// symbol => unique
+
+
+// console.log(typeof(id)); 
+// console.log(typeof(null));
+console.log(typeof(undefined));
+
+//------ premitive data type
+
+// Object
