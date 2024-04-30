8814bcfcc50b45c3b7f80d08422cb6edee55c696
shreya631 <74915529+shreya631@users.noreply.github.com>
2024-04-30 00:37:39 +0000

    conversionDataTypes

---
+++ basic/conversionOperation.js
@@ -0,0 +1,35 @@
+let score = 33;
+let score2 = "33";
+let score3 = "33abc";
+let score4 = null;
+let score5 = undefined;
+let score6 = true;
+
+// let valueInNumber = Number(score3);
+let valueInNumber = Number(score6);
+
+// console.log(typeof(score));
+// console.log(typeof(score2));
+// console.log(typeof (valueInNumber));
+// console.log(valueInNumber);
+
+
+// "33" => 33
+//"33abs" => nan
+// true => 1, false = 0
+
+
+// Boolean conversion
+
+// let isLoggedIn = 1
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn);
+
+// 1= true, 0 = false
+
+// "" => false
+
+// "Ram ji" => True
